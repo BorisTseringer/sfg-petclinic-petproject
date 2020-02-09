111c882f52c1b6a716809404fc96797e9e1df66c
taskurim <borystserinher@gmail.com>
2020-02-09 02:39:01 +0100

    Initial project add

---
+++ src/main/java/tseringer/boris/sfgpetclinicpetproject/SfgPetclinicPetprojectApplication.java
@@ -0,0 +1,13 @@
+package tseringer.boris.sfgpetclinicpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetclinicPetprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetclinicPetprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tseringer/boris/sfgpetclinicpetproject/SfgPetclinicPetprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package tseringer.boris.sfgpetclinicpetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetclinicPetprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
