dc6e204ccfa317bb7b3486807d14003dfc8c15aa
taskurim <borystserinher@gmail.com>
2020-02-18 00:49:24 +0100

    added base entity. closes #1

--- pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Person.java
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Person.java
@@ -1,6 +1,6 @@
 package tseringer.boris.sfgpetclinicpetproject.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Pet.java
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Pet.java
@@ -2,7 +2,7 @@ package tseringer.boris.sfgpetclinicpetproject.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
