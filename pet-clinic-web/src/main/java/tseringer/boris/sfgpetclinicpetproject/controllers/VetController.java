f54620a01d2779b8484a16069d7b1eb38a652087
taskurim <borystserinher@gmail.com>
2020-02-24 16:26:58 +0100

    added vet list and controller

--- pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/controllers/VetController.java
+++ pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/controllers/VetController.java
@@ -1,13 +1,23 @@
 package tseringer.boris.sfgpetclinicpetproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tseringer.boris.sfgpetclinicpetproject.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"vets", "vets/index", "vets/index.html"})
-    String indexVet(){
+    String listVet(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
