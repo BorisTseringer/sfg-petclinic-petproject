cc7961ef94dbd2eca912c8710d188eb0be48d315
taskurim <borystserinher@gmail.com>
2020-02-24 14:30:17 +0100

    added list owners

--- pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package tseringer.boris.sfgpetclinicpetproject.controllers;
 
+import io.micrometer.core.instrument.util.JsonUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tseringer.boris.sfgpetclinicpetproject.services.OwnerService;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"owners", "owners/index", "owners/index.html"})
-    String index(){
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"","/","/index", "/index.html"})
+    String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
