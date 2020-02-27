051b314d996b5334f50f7ad2a2224532be7a8b15
taskurim <borystserinher@gmail.com>
2020-02-27 15:54:41 +0100

    added pettype service and petype service map impl. closes #12

---
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package tseringer.boris.sfgpetclinicpetproject.services;
+
+import tseringer.boris.sfgpetclinicpetproject.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package tseringer.boris.sfgpetclinicpetproject.services.map;
+
+import org.springframework.stereotype.Service;
+import tseringer.boris.sfgpetclinicpetproject.model.PetType;
+import tseringer.boris.sfgpetclinicpetproject.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+}
