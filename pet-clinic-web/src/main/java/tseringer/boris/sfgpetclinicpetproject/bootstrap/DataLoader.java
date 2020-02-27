92caa0df4d3fc617ba739ce84858737101639a69
taskurim <borystserinher@gmail.com>
2020-02-27 09:45:33 +0100

    added dataloader

--- pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/bootstrap/DataLoader.java
@@ -1,5 +1,6 @@
 package tseringer.boris.sfgpetclinicpetproject.bootstrap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import tseringer.boris.sfgpetclinicpetproject.model.Owner;
@@ -15,39 +16,36 @@ public class DataLoader implements CommandLineRunner {
     private final PetMapService petMapService;
     private final VetMapService vetMapService;
 
-    public DataLoader() {
-        ownerMapService = new OwnerMapService();
-        petMapService = new PetMapService();
-        vetMapService = new VetMapService();
+    @Autowired // required in Spring 4
+    public DataLoader(OwnerMapService ownerMapService, PetMapService petMapService, VetMapService vetMapService) {
+        this.ownerMapService = ownerMapService;
+        this.petMapService = petMapService;
+        this.vetMapService = vetMapService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Ivan");
         owner1.setLastName("Ivanov");
         ownerMapService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
-        owner1.setFirstName("Petr");
-        owner1.setLastName("Petrov");
+        owner2.setFirstName("Petr");
+        owner2.setLastName("Petrov");
         ownerMapService.save(owner2);
 
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(3L);
         vet1.setFirstName("Evegen");
         vet1.setLastName("Badcom");
         vetMapService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(4L);
         vet2.setFirstName("Bodya");
-        vet1.setLastName("Bodyanov");
+        vet2.setLastName("Bodyanov");
         vetMapService.save(vet2);
 
         System.out.println("Loaded vets...");
