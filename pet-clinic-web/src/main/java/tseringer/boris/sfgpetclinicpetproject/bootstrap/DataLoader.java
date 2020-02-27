efb25e175a21bbb5287d30c0ce9a56fde7330c10
taskurim <borystserinher@gmail.com>
2020-02-27 19:22:21 +0100

    added pettypes in bootstrap

--- pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/tseringer/boris/sfgpetclinicpetproject/bootstrap/DataLoader.java
@@ -4,52 +4,78 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import tseringer.boris.sfgpetclinicpetproject.model.Owner;
+import tseringer.boris.sfgpetclinicpetproject.model.Pet;
+import tseringer.boris.sfgpetclinicpetproject.model.PetType;
 import tseringer.boris.sfgpetclinicpetproject.model.Vet;
-import tseringer.boris.sfgpetclinicpetproject.services.map.OwnerMapService;
-import tseringer.boris.sfgpetclinicpetproject.services.map.PetMapService;
-import tseringer.boris.sfgpetclinicpetproject.services.map.VetMapService;
+import tseringer.boris.sfgpetclinicpetproject.services.OwnerService;
+import tseringer.boris.sfgpetclinicpetproject.services.PetService;
+import tseringer.boris.sfgpetclinicpetproject.services.PetTypeService;
+import tseringer.boris.sfgpetclinicpetproject.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final OwnerMapService ownerMapService;
-    private final PetMapService petMapService;
-    private final VetMapService vetMapService;
+    private final OwnerService ownerService;
+    private final PetService petService;
+    private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired // required in Spring 4
-    public DataLoader(OwnerMapService ownerMapService, PetMapService petMapService, VetMapService vetMapService) {
-        this.ownerMapService = ownerMapService;
-        this.petMapService = petMapService;
-        this.vetMapService = vetMapService;
+    public DataLoader(OwnerService ownerService, PetService petService, VetService vetService, PetTypeService petTypeService) {
+        this.ownerService = ownerService;
+        this.petService = petService;
+        this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dogPetType = new PetType();
+        dogPetType.setName("dog");
+        petTypeService.save(dogPetType);
+
+        PetType catPetType = new PetType();
+        catPetType.setName("cat");
+        petTypeService.save(catPetType);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Ivan");
         owner1.setLastName("Ivanov");
-        ownerMapService.save(owner1);
+        ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Petr");
         owner2.setLastName("Petrov");
-        ownerMapService.save(owner2);
+        ownerService.save(owner2);
 
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Evegen");
         vet1.setLastName("Badcom");
-        vetMapService.save(vet1);
+        vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Bodya");
         vet2.setLastName("Bodyanov");
-        vetMapService.save(vet2);
+        vetService.save(vet2);
 
         System.out.println("Loaded vets...");
 
+        Pet pet1 = new Pet();
+        pet1.setBirthDate(LocalDate.now());
+        pet1.setOwner(owner1);
+        pet1.setPetType(dogPetType);
+        petService.save(pet1);
+
+        Pet pet2 = new Pet();
+        pet2.setBirthDate(LocalDate.now());
+        pet2.setOwner(owner2);
+        pet2.setPetType(catPetType);
+        petService.save(pet2);
 
      }
 }
