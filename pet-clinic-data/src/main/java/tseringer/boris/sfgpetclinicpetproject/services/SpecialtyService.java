669e3c6c4a129963f75f9770704c4255fa6dfdde
taskurim <borystserinher@gmail.com>
2020-02-28 01:06:18 +0100

    added specialty service and map impl. closes #13

---
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package tseringer.boris.sfgpetclinicpetproject.services;
+
+import tseringer.boris.sfgpetclinicpetproject.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package tseringer.boris.sfgpetclinicpetproject.services.map;
+
+import org.springframework.stereotype.Service;
+import tseringer.boris.sfgpetclinicpetproject.model.Specialty;
+import tseringer.boris.sfgpetclinicpetproject.services.SpecialtyService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+}
