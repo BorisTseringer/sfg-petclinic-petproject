6ab72e78491939df06a4d3c6571817080d95af67
taskurim <borystserinher@gmail.com>
2020-04-01 15:47:50 +0300

    project lombok anotations was added in model entities

--- pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Specialty.java
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Specialty.java
@@ -1,9 +1,19 @@
 package tseringer.boris.sfgpetclinicpetproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "specealities")
 public class Specialty extends BaseEntity {
@@ -11,11 +21,4 @@ public class Specialty extends BaseEntity {
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Vet.java
+++ pet-clinic-data/src/main/java/tseringer/boris/sfgpetclinicpetproject/model/Vet.java
@@ -1,9 +1,18 @@
 package tseringer.boris.sfgpetclinicpetproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -12,11 +21,4 @@ public class Vet extends Person {
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
-    public Set<Specialty> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
-    }
 }
